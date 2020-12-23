a946f77129d8cad5681fd306c700a055ade5911b
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-23 19:36:13 +0530

    Taking Inputs user case

--- sortingArithmetic.sh
+++ sortingArithmetic.sh
@@ -2,3 +2,9 @@
 
 echo "Welcome to Sorting Arithmetic Computation Program"
 echo -ne "\n"
+
+read -p "Enter First Number : " a
+read -p "Enter Second Number : " b
+read -p "Enter Third Number : " c
+
+echo "The Taken Inputs Are $a $b $c"
