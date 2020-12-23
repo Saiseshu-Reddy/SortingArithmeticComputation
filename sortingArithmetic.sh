c6681b9961da6bfbcd0c0f75057c6922b0d96476
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-23 19:49:23 +0530

    Stroring results in a dictionary

--- sortingArithmetic.sh
+++ sortingArithmetic.sh
@@ -18,3 +18,13 @@ echo "The Result is" $p
 echo "The Result is" $q
 echo "The Result is" $r
 echo "The result is" $s
+
+declare -A res	#saving results in a dictionary
+
+res[0]=$p
+res[1]=$q
+res[2]=$r
+res[3]=$s
+
+echo "Index Numbers : ${!res[@]}"
+echo "Index Numbers : ${res[@]}"
